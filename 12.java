6b59950d76074cb7a7d791ff6ec043d2ea9b5e44
wlemfWKd <dlwldn2424@naver.com>
2024-03-05 16:09:58 +0900

    짝수의 합

---
+++ 12.java
@@ -0,0 +1,23 @@
+//정수 n이 주어질때, n이하의 짝수를 모두 더한 값을 return 하도록 soultion함수를 작성하시오
+
+class Solution {
+
+    public int solution(int n) {
+
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 2 == 0) {
+
+                answer += i;
+
+            }
+
+        }
+
+        return answer;
+
+    }
+
+}

8985efc317e5644d6bc26c1fd6c81eac93107b71
wlemfWKd <dlwldn2424@naver.com>
2024-03-05 16:10:33 +0900

    배열의 평균값

---
+++ 13.java
@@ -0,0 +1,23 @@
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성하시오
+
+class Solution {
+
+    public double solution(int[] numbers) {
+
+        double answer = 0;
+
+        int result = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            result += numbers[i];
+
+        }
+
+        answer = (double) result / numbers.length;
+
+        return answer;
+
+    }
+
+}

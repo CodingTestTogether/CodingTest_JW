cd1be510f25381346c2ea4b89cacf3fd8d67f127
wlemfWKd <dlwldn2424@naver.com>
2024-03-05 16:06:26 +0900

    두 수의 합

---
+++ 7.java
@@ -0,0 +1,13 @@
+//정수 num1과 num2가 주어질때, num1과 num2의 합을 return하도록 solution 함수를 완성하시오
+
+class Solution {
+
+    public int solution(int num1, int num2) {
+
+        int answer = num1 + num2;
+
+        return answer;
+
+    }
+
+}
